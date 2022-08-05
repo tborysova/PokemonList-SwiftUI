b808cee6e73253556963abb4db2ac4cd91df4c6a
tetianaborysova <tetiana.borysova@empeek.io>
2022-08-05 13:54:02 +0200

    Initial Commit

---
+++ PokemonSwiftUI/PokemonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonSwiftUIApp.swift
+//  PokemonSwiftUI
+//
+//  Created by Tetiana Borysova on 05.08.22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
